81baa435ff85aa76abcd6ed859ba64d76a629f34
Sharon <sharon.febre@gmail.com>
2017-11-13 06:20:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function sayHiToGrandma (string) {
     return "I can't hear you!";
   }
 
- if (string === (string.toUpperCase()) 
-   return "YES INDEED!";
+ if (string === (string.toUpperCase())
+   return "YES, INDEED!";
  }
 
  if (string == "I love you, Grandma.") {
