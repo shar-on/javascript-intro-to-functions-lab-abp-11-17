5ea2598a560e909dea10ddd638b8fedc25907b89
Sharon <sharon.febre@gmail.com>
2017-11-13 06:04:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper (string) {
 }
 
 function sayHiToGrandma (string) {
-  if (string.toLowerCase()) === string) {
-    return "I can't hear you!"
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
   }
 
  if (string.toUperCase()) === string) {
