a589eaa6de276619424a8ee7bdd2c7de9a280799
Sharon <sharon.febre@gmail.com>
2017-11-13 06:39:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper (string) {
 
 
  function sayHiToGrandma (string) {
-   if (string === string.toLowerCase()) {
+   if (string === ('hello') {
      return "I can't hear you!";
      }
-     if (string === (string.toUpperCase()){
+     if (string === ('Hello'){
        return "YES INDEED!";
      }
      if (string === ("I love you, Grandma.") {
